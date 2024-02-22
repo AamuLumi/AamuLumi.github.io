db7464dd4e91b11aa53e8d827fe18d3fd30b0ac4
Florian Kauder <florian@kauder.fr>
2024-02-22 19:00:47 +0100

    feat(app): Add fix on fonts and ogImage and add new article

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -1,50 +1,64 @@
-import { i18n } from "../i18n"
-import { FullSlug, joinSegments, pathToRoot } from "../util/path"
-import { JSResourceToScriptElement } from "../util/resources"
-import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import { i18n } from '../i18n';
+import { FullSlug, joinSegments, pathToRoot } from '../util/path';
+import { JSResourceToScriptElement } from '../util/resources';
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from './types';
+
+const imagesExtensions = ['.png', '.jpg', '.jpeg', '.gif', '.bmp', '.webp'];
 
 export default (() => {
-  const Head: QuartzComponent = ({ cfg, fileData, externalResources }: QuartzComponentProps) => {
-    const title = fileData.frontmatter?.title ?? i18n(cfg.locale).propertyDefaults.title
-    const description =
-      fileData.description?.trim() ?? i18n(cfg.locale).propertyDefaults.description
-    const { css, js } = externalResources
+	const Head: QuartzComponent = ({ cfg, fileData, externalResources }: QuartzComponentProps) => {
+		const title = fileData.frontmatter?.title ?? i18n(cfg.locale).propertyDefaults.title;
+		const description =
+			fileData.description?.trim() ?? i18n(cfg.locale).propertyDefaults.description;
+		const { css, js } = externalResources;
 
-    const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
-    const path = url.pathname as FullSlug
-    const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
+		const url = new URL(`https://${cfg.baseUrl ?? 'example.com'}`);
+		const path = url.pathname as FullSlug;
+		const baseDir = fileData.slug === '404' ? path : pathToRoot(fileData.slug!);
 
-    const iconPath = joinSegments(baseDir, "static/icon.png")
-    const ogImagePath = `https://${cfg.baseUrl}/static/og-image.png`
+		const iconPath = joinSegments(baseDir, 'static/icon.png');
 
-    return (
-      <head>
-        <title>{title}</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        {cfg.baseUrl && <meta property="og:image" content={ogImagePath} />}
-        <meta property="og:width" content="1200" />
-        <meta property="og:height" content="675" />
-        <link rel="icon" href={iconPath} />
-        <meta name="description" content={description} />
-        <meta name="generator" content="Quartz" />
-        {cfg.theme.cdnCaching && (
-          <>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
-          </>
-        )}
-        {css.map((href) => (
-          <link key={href} href={href} rel="stylesheet" type="text/css" spa-preserve />
-        ))}
-        {js
-          .filter((resource) => resource.loadTime === "beforeDOMReady")
-          .map((res) => JSResourceToScriptElement(res, true))}
-      </head>
-    )
-  }
+		let headerImage = fileData.frontmatter?.titleImage as string;
+		let ogImagePath = `https://${cfg.baseUrl}/static/og-image.png`;
 
-  return Head
-}) satisfies QuartzComponentConstructor
+		if (headerImage) {
+			const ext = headerImage.substring(headerImage.lastIndexOf('.'));
+
+			if (imagesExtensions.includes(ext)) {
+				headerImage = `${headerImage.substring(0, headerImage.lastIndexOf('.'))}.webp`;
+			}
+
+			ogImagePath = `https://${cfg.baseUrl}/_assets/headers/${encodeURIComponent(headerImage)}`;
+		}
+
+		return (
+			<head>
+				<title>{title}</title>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<meta property="og:title" content={title} />
+				<meta property="og:description" content={description} />
+				{cfg.baseUrl && <meta property="og:image" content={ogImagePath} />}
+				<meta property="og:width" content="1200" />
+				<meta property="og:height" content="675" />
+				<link rel="icon" href={iconPath} />
+				<meta name="description" content={description} />
+				<meta name="generator" content="Quartz" />
+				{cfg.theme.cdnCaching && (
+					<>
+						<link rel="preconnect" href="https://fonts.googleapis.com" />
+						<link rel="preconnect" href="https://fonts.gstatic.com" />
+					</>
+				)}
+				{css.map((href) => (
+					<link key={href} href={href} rel="stylesheet" type="text/css" spa-preserve />
+				))}
+				{js
+					.filter((resource) => resource.loadTime === 'beforeDOMReady')
+					.map((res) => JSResourceToScriptElement(res, true))}
+			</head>
+		);
+	};
+
+	return Head;
+}) satisfies QuartzComponentConstructor;
